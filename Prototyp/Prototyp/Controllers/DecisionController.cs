1db185176aecd6424fa826894bf8198088d32d13
Christian <chr.goldberger@gmail.com>
2018-01-03 14:09:28 +0100

    New Decision design und funktionstechnisch fertiggestellt.

--- Prototyp/Prototyp/Controllers/DecisionController.cs
+++ Prototyp/Prototyp/Controllers/DecisionController.cs
@@ -8,70 +8,101 @@ using System.Web.Mvc;
 
 namespace Prototyp.Controllers
 {
-    public class DecisionController : Controller
-    {
-        //List<QuestionControl> qControls = new List<QuestionControl>();
-        List<QuestionAnswer> qAnswers = new List<QuestionAnswer>();
-        List<Question> questions;
+	public class DecisionController : Controller
+	{
+		//List<QuestionControl> qControls = new List<QuestionControl>();
+		List<QuestionAnswer> qAnswers = new List<QuestionAnswer>();
+		List<Question> questions;
 
-        string title;
-        string description;
+		string title;
+		string description;
 
-        Decision decision = null;
+		Decision decision = null;
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Index([Bind(Include = "Title,Description")] Decision d)
-        {
-            if (ModelState.IsValid)
-            {
-                title = d.Title;
-                description = d.Description;
+		public JsonResult SaveDecision(string title, string description)
+		{
+			int check = 0; //0...Entscheidung erfolgreich gespeichert //1...Kein Titel eingefügt //2...Keine Beschreibung eingefügt //3...Titel und Beschreibung fehlen
+			string desc = description.Trim(' '); //manchmal wird " ssadasd" gespeichert, deshalb trim
+			if (title.Equals("") && desc.Equals(""))
+			{
+				return Json(3, JsonRequestBehavior.AllowGet);
+			}
+			if (title.Equals("")) return Json(1, JsonRequestBehavior.AllowGet);
+			if (desc.Equals("")) return Json(2, JsonRequestBehavior.AllowGet);
 
-                if (this.decision == null)
-                {
-                    questions = QuestionProvider.GetQuestionProvider().GetQuestions();
-                }
-                else
-                {
-                    questions = QuestionProvider.GetQuestionProvider().GetQuestions().Where(qq => this.decision.Answers.FirstOrDefault(dd => dd.QuestionKey == qq.Key) != null).ToList();
-                }
+			if (this.decision == null)
+			{
+				questions = QuestionProvider.GetQuestionProvider().GetQuestions();
+			}
+			else
+			{
+				questions = QuestionProvider.GetQuestionProvider().GetQuestions().Where(qq => this.decision.Answers.FirstOrDefault(dd => dd.QuestionKey == qq.Key) != null).ToList();
+			}
 
-                //Question in QuestionAnswer wandeln
-                foreach (Question qqq in questions)
-                {
-                    QuestionAnswer qa = new QuestionAnswer();
-                    qa.QuestionKey = qqq.Text;
-                    qAnswers.Add(qa);
-                }
-                Session["qAnswers"] = qAnswers;
+			//Question in QuestionAnswer wandeln
+			foreach (Question qqq in questions)
+			{
+				QuestionAnswer qa = new QuestionAnswer();
+				qa.QuestionKey = qqq.Text;
+				qAnswers.Add(qa);
+			}
+			Session["qAnswers"] = qAnswers;
 
-                return RedirectToAction("New");
-            }
-            return View();
-        }
+			return Json(check, JsonRequestBehavior.AllowGet);
+		}
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public ActionResult Index([Bind(Include = "Title,Description")] Decision d)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		title = d.Title;
+		//		description = d.Description;
 
-        public ActionResult New(int? q)
-        {
-            //Ab hier weiter mit QuestionAnswer arbeiten (enthält Rating und Argumente)
-            qAnswers = (List<QuestionAnswer>)Session["qAnswers"];
-            QuestionAnswer myQ = qAnswers.ElementAt(q - 1 ?? 0);
-            ViewBag.myQ = myQ;
+		//		if (this.decision == null)
+		//		{
+		//			questions = QuestionProvider.GetQuestionProvider().GetQuestions();
+		//		}
+		//		else
+		//		{
+		//			questions = QuestionProvider.GetQuestionProvider().GetQuestions().Where(qq => this.decision.Answers.FirstOrDefault(dd => dd.QuestionKey == qq.Key) != null).ToList();
+		//		}
 
-            int pageSize = 1;
-            int pageNumber = (q ?? 1);
+		//		//Question in QuestionAnswer wandeln
+		//		foreach (Question qqq in questions)
+		//		{
+		//			QuestionAnswer qa = new QuestionAnswer();
+		//			qa.QuestionKey = qqq.Text;
+		//			qAnswers.Add(qa);
+		//		}
+		//		Session["qAnswers"] = qAnswers;
 
-            return View(qAnswers.ToPagedList(pageNumber, pageSize));
-        }
+		//		return RedirectToAction("New");
+		//	}
+		//	return View();
+		//}
 
-        public ActionResult Show()
-        {
-            return View();
-        }
-    }
+		public ActionResult New(int? q)
+		{
+			//Ab hier weiter mit QuestionAnswer arbeiten (enthält Rating und Argumente)
+			qAnswers = (List<QuestionAnswer>)Session["qAnswers"];
+			QuestionAnswer myQ = qAnswers.ElementAt(q - 1 ?? 0);
+			ViewBag.myQ = myQ;
+
+			int pageSize = 1;
+			int pageNumber = (q ?? 1);
+
+			return View(qAnswers.ToPagedList(pageNumber, pageSize));
+		}
+
+		public ActionResult Show()
+		{
+			return View();
+		}
+	}
 }
--- Prototyp/Prototyp/Scripts/newDecision.js
+++ Prototyp/Prototyp/Scripts/newDecision.js
@@ -6,5 +6,28 @@ function saveDecision()
 {
 	var title = $("#title").val();
 	var description = $("#description").val();
+	var url = "Decision/SaveDecision"
+	$.getJSON(url, { title: title, description: description }, function (check) {
+		if (check == 0)
+		{
+			window.location.href = "/Decision/New";
+			$("#titleForm").removeClass("has-error");
+			$("#descriptionForm").removeClass("has-error");
+		}
+		else if (check == 1)
+		{
+			$("#titleForm").addClass("has-error");
+		}
+		else if (check == 2)
+		{
+			$("#descriptionForm").addClass("has-error");
+		}
+		else if (check == 3)
+		{
+			$("#titleForm").addClass("has-error");
+			$("#descriptionForm").addClass("has-error");
+		}
+
+	});
 
 }
