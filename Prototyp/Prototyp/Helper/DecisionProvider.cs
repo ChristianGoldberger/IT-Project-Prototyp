82de2339afbf90ceda3fdbc52a5956733b23e7ff
ChrisitanGoldberger <34503123+PatrickSchmutz@users.noreply.github.com>
2018-01-18 19:55:59 +0100

    Fragen Speichern

--- Prototyp/Prototyp/Helper/DecisionProvider.cs
+++ Prototyp/Prototyp/Helper/DecisionProvider.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web.Hosting;
-using System.Xml.Serialization;
+using System.Xml.Serialization; 
 
 namespace Helper {
     class DecisionProvider : IDecisionProvider {
@@ -13,6 +13,11 @@ namespace Helper {
         static DecisionProvider provider = null;
 
         List<Decision> decisions;
+        List<Decision> decisionsHelper= new List<Decision>();
+        List<int> valuesRadioButton = new List<int>();
+
+        private int id = 1;
+
 
         private DecisionProvider() {
             decisions = new List<Decision>();
@@ -39,10 +44,46 @@ namespace Helper {
             return decisions;
         }
 
+        public int GetId(){
+            return this.id;
+        }
+
+        public void setRadioValue(int i){
+            this.valuesRadioButton.Add(i);
+        }
+
+        public void clearRadioValue(){
+            this.valuesRadioButton.Clear();
+        }
+
+        public List<int> getRadioValue(){
+            return this.valuesRadioButton;
+        }
+
+        public void Delete(int id){
+            
+
+            for (int i = 0; i < decisions.Count(); i++){
+                if(decisions.ElementAt(i).Id.Equals(id)){
+                    decisions.RemoveAt(i);
+                }
+            }
+
+        }
+
+        public void AddDecisionHelper(Decision d){
+            this.decisionsHelper.Add(d);
+        }
+        public Decision getDecisionHelper(){
+            Decision d = this.decisionsHelper.ElementAt(0);
+            this.decisionsHelper.RemoveAt(0);
+            return d;
+        }
+       
         public void Add(Decision decision) {
             decision.Id =  decisions.Count == 0 ? 0 : decisions.Max(d => d.Id) + 1;
             decisions.Add(decision);
-
+            this.id++;
             File.Delete(D_PATH);
             File.WriteAllText(D_PATH, ToXML(decisions));
         }
