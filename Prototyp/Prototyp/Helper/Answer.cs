0087b5caee215b09fe5c170694594765d05e8a27
Christian <chr.goldberger@gmail.com>
2018-01-18 18:02:57 +0100

    Details bearbeitet, Fehler vom Ernst ausgebessert. Neues Objekt Answer erzeugt um die Detail korrekt anzuzeigen.

---
+++ Prototyp/Prototyp/Helper/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Helper;
+
+namespace Prototyp.Helper
+{
+	public class Answer
+	{
+		public int Rating { get; set; }
+
+		public String AnswerText { get; set; }
+		public IntuitionEffect AnswerEffect { get; set; }
+	}
+}
